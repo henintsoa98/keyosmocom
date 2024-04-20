233597691f3a5ca4cc712a529c382c0d993ec469
debian <debian@localhost>
2024-04-20 10:33:17 +0300

    update setup.bash

--- docker_open5gs_build/setup.bash
+++ docker_open5gs_build/setup.bash
@@ -2,6 +2,8 @@
 source color
 
 function RUNNING() {
+	echo -e "${BRed}Running :"
+	echo -e "${BCyan}$(cat COMMAND)${Reset}"
 	bash ./COMMAND
 	rm -f COMMAND
 }
@@ -16,8 +18,8 @@ DOCKER_IP=$(ip address show docker0 | grep "inet " | sed "s#/# #" | awk '{print 
 
 #APN
 echo -e "${BYellow}=> APN <=${Reset}"
-echo "curl -X 'PUT' 'http://DOCKER_IP:8080/apn/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"apn\": \"internet\", \"apn_ambr_dl\": 0, \"apn_ambr_ul\": 0 }'" > COMMAND;RUNNING
-echo "curl -X 'PUT' 'http://DOCKER_IP:8080/apn/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"apn\": \"ims\", \"apn_ambr_dl\": 0, \"apn_ambr_ul\": 0 }'" > COMMAND;RUNNING
+echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/apn/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"apn\": \"internet\", \"apn_ambr_dl\": 0, \"apn_ambr_ul\": 0 }'" > COMMAND;RUNNING
+echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/apn/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"apn\": \"ims\", \"apn_ambr_dl\": 0, \"apn_ambr_ul\": 0 }'" > COMMAND;RUNNING
 echo
 echo -e "${BRed}=> OTHER <=${Reset}"
 MSISDN="MSISDN 00000001 00000002 00000003 00000004 00000005 00000006 00000007 00000008 00000009 00000010"
@@ -44,14 +46,13 @@ for ID in $MSISDN ; do
 		echo -e "${BYellow}=> AUC $msisdn <=${Reset}"
 		echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/auc/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"ki\": \"$ki\", \"opc\": \"$opc\", \"amf\": \"8000\", \"sqn\": 0, \"imsi\": \"$imsi\" }'" > COMMAND;RUNNING
 		# AUC_ID, NEED BY NEXT STEP
-		echo "curl -X 'GET' 'http://$DOCKER_IP:8080/auc/imsi/$imsi' -H 'accept: application/json' | awk '{print \$8}'| sed \"s#,##\"" > COMMAND
-		auc_id=$(RUNNING)
-		#echo -e $BRed$auc_id$Reset
+		# THIS WAS A BIG ERROR # echo "curl -X 'GET' 'http://$DOCKER_IP:8080/auc/imsi/$imsi' -H 'accept: application/json' | json_pp | grep auc_id | sed 's#[ \"auc_id,:]*##g'" > COMMAND
+		auc_id=$(curl -X 'GET' "http://$DOCKER_IP:8080/auc/imsi/$imsi" -H 'accept: application/json' | json_pp | grep auc_id | sed 's#[ \"auc_id,:]*##g')
 		
-		echo -e "${BYellow}=> subscriber $msisdn <=${Reset}"
-		echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/subscriber/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"imsi\": \"$imsi\", \"enabled\": true, \"auc_id\": $auc_id, \"default_apn\": $APN_INTERNET, \"apn_list\": \"$APN_INTERNET,$APN_IMS\", \"msisdn\": \"$msisdn\", "ue_ambr_dl": 0, "ue_ambr_ul": 0 }'" > COMMAND;RUNNING
+		echo -e "${BYellow}=> subscriber $msisdn auc_id $auc_id <=${Reset}"
+		echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/subscriber/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"imsi\": \"$imsi\", \"enabled\": true, \"auc_id\": ${auc_id}, \"default_apn\": $APN_INTERNET, \"apn_list\": \"$APN_INTERNET,$APN_IMS\", \"msisdn\": \"$msisdn\", \"ue_ambr_dl\": 0, \"ue_ambr_ul\": 0 }'" > COMMAND;RUNNING
 		
 		echo -e "${BYellow}=> ims_subscriber $msisdn <=${Reset}"
-		echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/ims_subscriber/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"imsi\": \"$imsi\", \"msisdn\": \"$msisdn\", \"sh_profile\": \"string\", \"scscf_peer\": \"scscf.ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org\", \"msisdn_list\": \"[${msisdn}]\", \"ifc_path\": \"default_ifc.xml\", \"scscf\": \"sip:scscf.ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org:6060\", \"scscf_realm\": \"ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org\" }'"
+		echo "curl -X 'PUT' 'http://$DOCKER_IP:8080/ims_subscriber/' -H 'accept: application/json' -H 'Content-Type: application/json' -d '{ \"imsi\": \"$imsi\", \"msisdn\": \"$msisdn\", \"sh_profile\": \"string\", \"scscf_peer\": \"scscf.ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org\", \"msisdn_list\": \"[${msisdn}]\", \"ifc_path\": \"default_ifc.xml\", \"scscf\": \"sip:scscf.ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org:6060\", \"scscf_realm\": \"ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org\" }'" > COMMAND;RUNNING
 	fi
 done
